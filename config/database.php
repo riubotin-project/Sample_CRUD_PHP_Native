320afeea0f6b302f88b03a34d521895fe4e83390
Riubotin <40457741+riubotin-project@users.noreply.github.com>
2023-11-13 14:49:14 +0700

    update
    update

--- config/database.php
+++ config/database.php
@@ -1,9 +1,9 @@
 <?php
 // used to connect to the database
-$host = "fdb21.awardspace.net";
+$host = "localhost";
 $db_name = "2747414_database";
-$username = "2747414_database";
-$password = "2747414_database";
+$username = "root";
+$password = "";
   
 try {
     $con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
