12d032c68001a84a116a77d93eb97ad628479bdd
Riubotin <40457741+riubotin-project@users.noreply.github.com>
2023-11-13 15:13:32 +0700

    update
    update

--- config/database.php
+++ config/database.php
@@ -1,9 +1,9 @@
 <?php
 // used to connect to the database
-$host = "localhost";
-$db_name = "2747414_database";
-$username = "root";
-$password = "";
+$host = "kiouni.db.elephantsql.com";
+$db_name = "tiwormkn";
+$username = "tiwormkn";
+$password = "h3J0Mjbin1wd3d0H5OSjjHvEkVHgDKNp";
   
 try {
     $con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
